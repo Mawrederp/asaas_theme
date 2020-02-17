ea9bcbd34454ec7c1504bfc2608b99f8455017c9
ammarhararah <ammarhararah@gmail.com>
2020-02-17 10:42:26 +0200

    fix image display

--- asaas_theme/public/js/page.js
+++ asaas_theme/public/js/page.js
@@ -274,10 +274,59 @@ frappe.ui.toolbar.Toolbar = frappe.ui.toolbar.Toolbar.extend({
 		});
 	}
 });
+frappe.new_avatar = function(user, css_class, title) {
+	if(user) {
+		// desk
+		var user_info = frappe.user_info(user);
+	} else {
+		// website
+		user_info = {
+			image: frappe.get_cookie("user_image"),
+			fullname: frappe.get_cookie("full_name"),
+			abbr: frappe.get_abbr(frappe.get_cookie("full_name")),
+			color: frappe.get_palette(frappe.get_cookie("full_name"))
+		}
+	}
+
+	if(!title) {
+		title = user_info.fullname;
+	}
+
+	if(!css_class) {
+		css_class = "avatar-small";
+	}
+
+	if(user_info.image) {
+
+		var image = (window.cordova && user_info.image.indexOf('http')===-1) ?
+			frappe.base_url + user_info.image : user_info.image;
+
+		return repl(`<span class="avatar %(css_class)s" title="%(title)s">\
+			<span class="avatar-frame" style="background-image: url('%(image)s')"\
+			title="%(title)s"></span></span>`, {
+				image: image,
+				title: title,
+				abbr: user_info.abbr,
+				css_class: css_class
+			});
+	} else {
+		var abbr = user_info.abbr;
+		if(css_class==='avatar-small' || css_class=='avatar-xs') {
+			abbr = abbr.substr(0, 1);
+		}
+		return repl('<span class="avatar %(css_class)s" title="%(title)s">\
+			<div class="standard-image" style="background-color: %(color)s;">%(abbr)s</div></span>', {
+				title: title,
+				abbr: abbr,
+				css_class: css_class,
+				color: user_info.color
+			})
+	}
+}
 frappe.ui.toolbar.Toolbar = frappe.ui.toolbar.Toolbar.extend({
 	init: function() {
 		$('header').append(frappe.render_template("navbar", {
-			avatar: frappe.avatar(frappe.session.user)
+			avatar: frappe.new_avatar(frappe.session.user)
 		}));
 		$('.dropdown-toggle').dropdown();
 
